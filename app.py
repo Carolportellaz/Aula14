873cdee76e7ef0e77c4e761acd862ac3d91f3fda
Carol <carolportellazanelato@gmail.com>
2025-07-14 09:41:42 -0300

    commit web

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def carrega_modelo():
     #https://drive.google.com/file/d/1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp/view?usp=sharing
     url = 'https://drive.google.com/uc?id=1GpWix8dp6FeFAs6g0etbnw_avu9Aflfp'
     
+    
     gdown.down(url, 'modelo_quantizado16bits.tflite')
     
     interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')

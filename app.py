c7ebf4d090eac22286e2b01339bcf4a2f8f64d5e
Carol <carolportellazanelato@gmail.com>
2025-07-14 08:59:10 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title = "Classifica Folha de Videeiras"
+    )
+    
+    st.write("#Classificação Folha de Videiras")
+    
+if __name__ == "__main__":
+    main()
